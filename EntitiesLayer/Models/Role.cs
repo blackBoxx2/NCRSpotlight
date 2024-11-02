306f378c5bbbbc9d98dbe4f5e0487a006a559986
Andrew Stefanac <astefanac22@gmail.com>
2024-11-01 22:51:17 -0400

    Add Role Model

---
+++ EntitiesLayer/Models/Role.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntitiesLayer.Models
+{
+    internal class Role
+    {
+
+        [Key]
+        public int RoleID { get; set; }
+
+        [Required(ErrorMessage = "Must include a name for a role")]
+        [MaxLength(30)]
+        public string RoleName { get; set; }
+
+    }
+}
