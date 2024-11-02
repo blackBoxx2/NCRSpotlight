20d913a4da24f9d8553b36a8e684ecb82e693e6f
Andrew Stefanac <astefanac22@gmail.com>
2024-11-01 23:09:16 -0400

    Add Supplier Model

---
+++ EntitiesLayer/Models/Supplier.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntitiesLayer.Models
+{
+    internal class Supplier
+    {
+        [Key]
+        public int ID { get; set; }
+
+        [Required(ErrorMessage = "A supplier name is required")]
+        public string SupplierName { get; set; }
+
+        
+
+    }
+}
