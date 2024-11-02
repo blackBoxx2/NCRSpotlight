5bb97454519af4998cff712206a6fb660aefdc69
Andrew Stefanac <astefanac22@gmail.com>
2024-11-01 23:20:13 -0400

    Add Product Model

---
+++ EntitiesLayer/Models/Product.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntitiesLayer.Models
+{
+    internal class Product
+    {
+        [Key]
+        public int ID { get; set; }
+
+        [Required]
+        public int SupplierID { get; set; }
+
+        public Supplier? Supplier { get; set; }
+
+        [Required]
+        public string ProductNumber { get; set; }
+
+        [Required]
+        public string Description { get; set; }
+
+        [Required]
+        public byte[] Picture { get; set; }
+
+    }
+}
