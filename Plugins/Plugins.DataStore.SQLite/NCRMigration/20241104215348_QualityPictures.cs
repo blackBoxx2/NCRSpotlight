a6ead313cc428f784f26999b06defe0a2e56ed76
Andrew Stefanac <astefanac22@gmail.com>
2024-11-04 16:59:37 -0500

    add file support for quality portions

---
+++ Plugins/Plugins.DataStore.SQLite/NCRMigration/20241104215348_QualityPictures.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Plugins.DataStore.SQLite.NCRMigration
+{
+    /// <inheritdoc />
+    public partial class QualityPictures : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DefectPicture",
+                table: "QualityPortions");
+
+            migrationBuilder.AddColumn<int>(
+                name: "QualityPortionID",
+                table: "UploadedFile",
+                type: "INTEGER",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UploadedFile_QualityPortionID",
+                table: "UploadedFile",
+                column: "QualityPortionID");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_UploadedFile_QualityPortions_QualityPortionID",
+                table: "UploadedFile",
+                column: "QualityPortionID",
+                principalTable: "QualityPortions",
+                principalColumn: "ID",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_UploadedFile_QualityPortions_QualityPortionID",
+                table: "UploadedFile");
+
+            migrationBuilder.DropIndex(
+                name: "IX_UploadedFile_QualityPortionID",
+                table: "UploadedFile");
+
+            migrationBuilder.DropColumn(
+                name: "QualityPortionID",
+                table: "UploadedFile");
+
+            migrationBuilder.AddColumn<byte[]>(
+                name: "DefectPicture",
+                table: "QualityPortions",
+                type: "BLOB",
+                nullable: false,
+                defaultValue: new byte[0]);
+        }
+    }
+}
