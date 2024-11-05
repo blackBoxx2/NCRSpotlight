9cfcb0cacb01fc55c9edbf658e734dd5ed468a0a
Andrew Stefanac <astefanac22@gmail.com>
2024-11-05 10:17:28 -0500

    Add NCRLog interfaces -Andrew

---
+++ UseCasesLayer/UseCaseInterfaces/NCRLogUseCaseInterfaces/IGetNCRLogByIDAsyncUseCase.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UseCasesLayer.UseCaseInterfaces.NCRLogUseCaseInterfaces
+{
+    public interface IGetNCRLogByIDAsyncUseCase
+    {
+        Task Execute(int? id);
+    }
+}
