e9d94a921f1cea63114b744698ab177b899e415f
Andrew Stefanac <astefanac22@gmail.com>
2024-11-18 23:19:33 -0500

    add ncrphase field

---
+++ Plugins/Plugins.DataStore.SQLite/ncrcontext/20241119041300_AddNCRPhase.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Plugins.DataStore.SQLite.ncrcontext
+{
+    /// <inheritdoc />
+    public partial class AddNCRPhase : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Phase",
+                table: "NCRLog",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Phase",
+                table: "NCRLog");
+        }
+    }
+}
